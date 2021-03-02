c547743ef5314eb889171f8806b59f716939aaca
Lalith <lalithkmr94@gmail.com>
2021-03-02 18:16:06 +0530

    Adding notes and adding csvfiles storage folder

--- server/server.js
+++ server/server.js
@@ -15,10 +15,10 @@ app.start = function() {
     logger.info('App Started');
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    //console.log('Web server listening at: %s', baseUrl);
+    console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
-      //console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
       console.log(' ----------------------------------------------');
       console.log('|                                              |');
       console.log('|         Server Started successfully!         |');
