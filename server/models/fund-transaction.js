7d1d091134f2d0d0e4bad773461339eb2f9415f8
Lalith Kumar <lalithkmr94@gmail.com>
2021-06-13 20:59:59 +0530

    CashBook api changes

--- server/models/fund-transaction.js
+++ server/models/fund-transaction.js
@@ -96,7 +96,7 @@ module.exports = function(FundTransaction) {
     FundTransaction._cashInApi = (apiParams) => {
         return new Promise( async (resolve, reject) => {
             let userId =await  utils.getStoreOwnerUserId(apiParams.accessToken);
-            let queryValues = [userId, FUND_HOUSE_ID_MAP[apiParams.fundHouse], dateformat(apiParams.transactionDate, 'yyyy-mm-dd HH:MM:ss'), apiParams.amount, apiParams.remarks];
+            let queryValues = [userId, FUND_HOUSE_ID_MAP[apiParams.fundHouse], dateformat(apiParams.transactionDate, 'yyyy-mm-dd HH:MM:ss'), apiParams.amount, apiParams.category, apiParams.remarks];
             FundTransaction.dataSource.connector.query(SQL.CASH_TRANSACTION, queryValues, (err, res) => {
                 if(err){
                     reject(err);
@@ -121,7 +121,7 @@ module.exports = function(FundTransaction) {
     FundTransaction._cashOutApi = (apiParams) => {
         return new Promise( async (resolve, reject) => {
             let userId =await  utils.getStoreOwnerUserId(apiParams.accessToken);
-            let queryValues = [userId, FUND_HOUSE_ID_MAP[apiParams.fundHouse], dateformat(apiParams.transactionDate, 'yyyy-mm-dd HH:MM:ss'), (-apiParams.amount), apiParams.remarks];
+            let queryValues = [userId, FUND_HOUSE_ID_MAP[apiParams.fundHouse], dateformat(apiParams.transactionDate, 'yyyy-mm-dd HH:MM:ss'), (-apiParams.amount), apiParams.category, apiParams.remarks];
             FundTransaction.dataSource.connector.query(SQL.CASH_TRANSACTION, queryValues, (err, res) => {
                 if(err){
                     reject(err);
@@ -147,28 +147,133 @@ module.exports = function(FundTransaction) {
         );
     }
 
+    FundTransaction._appendFilters = (sql, params, identifier) => {
+        let filters = [];
+        switch(identifier) {
+            case 'FETCH_TRANSACTION_LIST':
+                if(params._userId)
+                    filters.push(`user_id=${params._userId}`);
+                if(params.fundHouse)
+                    filters.push(`fund_houses.name like '${params.fundHouse}%'`);
+                if(params.category)
+                    filters.push(`category like '${params.category}%'`);
+                if(params.startDate && params.endDate)
+                    filters.push(`(created_date BETWEEN '${params.startDate}' AND '${params.endDate}')`);
+                
+                let orderPart = '';
+                if(params.orderCol && params.orderBy) {
+                    if(params.orderCol == 'TRN_DATE')
+                        orderPart = ` ORDER BY transaction_date ${params.orderBy}`;
+                    else if(params.orderCol == 'CREATED_DATE')
+                        orderPart = ` ORDER BY created_date ${params.orderBy}`;
+                    else if(params.orderCol == 'MODIFIED_DATE')
+                        orderPart = ` ORDER BY modified_date ${params.orderBy}`;
+                }
+
+                if(filters.length > 0)
+                    sql += ` WHERE ${filters.join(` AND `)}`;
+
+                sql += orderPart;
+
+                let limit = params.offsetEnd - params.offsetStart;
+                sql += ` LIMIT ${limit} OFFSET ${params.offsetStart}`;
+                break;
+            case 'TRANSACTION_LIST_COLLECTIONS':
+                if(params._userId)
+                    filters.push(`user_id=${params._userId}`);
+                if(params.startDate && params.endDate)
+                    filters.push(`(created_date BETWEEN '${params.startDate}' AND '${params.endDate}')`);
+                if(filters.length > 0)
+                    sql += ` WHERE ${filters.join(` AND `)}`;
+                break;
+        }
+        return sql;
+    }
+
     FundTransaction._fetchTransactionsApi = (accessToken, params) => {
         return new Promise(async (resolve, reject) => {
-            let userId =await  utils.getStoreOwnerUserId(accessToken);
-            FundTransaction.dataSource.connector.query(SQL.TRANSACTION_LIST, [userId, dateformat(params.startDate, 'yyyy-mm-dd HH:MM:ss'), dateformat(params.endDate, 'yyyy-mm-dd HH:MM:ss')], (err, res) => {
-                if(err) {
-                    return reject(err);
-                } else {
-                    return resolve(res);
-                }
+            params._userId =await  utils.getStoreOwnerUserId(accessToken);
+            console.log(params.startDate); // 2021-05-14T18:30:00.000Z
+            console.log(dateformat(params.startDate, 'yyyy-mm-dd HH:MM:ss')); // 2021-05-15 00:00:00
+            
+            
+            let sql = SQL.TRANSACTION_LIST;
+            sql = FundTransaction._appendFilters(sql, params, 'FETCH_TRANSACTION_LIST');
+            let fetchPaginatedList = new Promise((resolve, reject) => {
+                FundTransaction.dataSource.connector.query(sql, (err, res) => {
+                    if(err) {
+                        return reject(err);
+                    } else {
+                        return resolve(res);
+                    }
+                });
             });
+
+            let totalSql = SQL.TRANSACTION_LIST_COLLECTIONS;
+            totalSql = FundTransaction._appendFilters(totalSql, params, 'TRANSACTION_LIST_COLLECTIONS');
+            let fetchCollections = new Promise((resolve, reject) => {
+                FundTransaction.dataSource.connector.query(totalSql, (err, res) => {
+                    if(err) {
+                        return reject(err);
+                    } else {
+                        return resolve(res);
+                    }
+                });
+            });
+
+            Promise.all([fetchPaginatedList, fetchCollections]).then(
+                (results) => {
+                    let obj = {
+                        results: results[0],
+                        collections: FundTransaction._constructCollections(results[1])
+                    }
+                    resolve(obj);
+                },
+                (error) => {
+                    reject(error);
+                }
+            )
+            .catch(
+                (exception) => {
+                    reject(exception);
+                }
+            );
         });
+    }
+
+    FundTransaction._constructCollections = (collRes) => {
+        let collections = {
+            count: collRes.length,
+            fundHouses: [],
+            categories: []
+        };
+        
+        if(collections.count > 0) {
+            _.each(collRes, (aColl, index) => {
+                if(aColl.name && collections.fundHouses.indexOf(aColl.name) == -1)
+                    collections.fundHouses.push(aColl.name);
+                if(aColl.category && collections.categories.indexOf(aColl.category) == -1)
+                    collections.categories.push(aColl.category);
+            });
+        }
+        return collections;
     }
 }
 
 let SQL = {
-    CASH_TRANSACTION: `INSERT INTO fund_transactions (user_id, fund_house_id, transaction_date, amount, remarks) VALUES (?,?,?,?,?)`,
+    CASH_TRANSACTION: `INSERT INTO fund_transactions (user_id, fund_house_id, transaction_date, amount, category, remarks) VALUES (?,?,?,?,?,?)`,
     TRANSACTION_LIST: `SELECT 
                             fund_houses.name AS fund_house_name,
                             fund_transactions.*
                         FROM
                             fund_transactions
                                 LEFT JOIN
-                            fund_houses ON fund_transactions.fund_house_id = fund_houses.id
-                        WHERE user_id=? AND (created_date BETWEEN ? AND ?);`
+                            fund_houses ON fund_transactions.fund_house_id = fund_houses.id`,
+    TRANSACTION_LIST_COLLECTIONS: `SELECT
+                                        category,
+                                        fund_houses.name
+                                    FROM
+                                        fund_transactions
+                                            LEFT JOIN
+                                        fund_houses ON fund_transactions.fund_house_id = fund_houses.id`,
 }
