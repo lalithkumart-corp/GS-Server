5fa5dbc02b70700fa5a76eece396e2d15c79a71c
lalithkumart <lalithkmr94@gmail.com>
2022-05-10 11:42:03 +0530

    NOTES Module - API Updates.

--- server/models/note.js
+++ server/models/note.js
@@ -3,19 +3,23 @@ let _ = require('lodash');
 let utils = require('../utils/commonUtils');
 
 module.exports = function(Note) {
-    Note.fetchByCustomerId = async (accessToken, customerId) => {
+    Note.fetchByCustomerId = async (accessToken, customerId, includeArchived) => {
         try{
-            let bucket = await Note.prototype.fetchNotes(accessToken, customerId);
+            let bucket = await Note.prototype.fetchNotes(accessToken, customerId, includeArchived);
             return {STATUS: 'SUCCESS', DATA: bucket};
         } catch(e) {
             return {STATUS: 'ERROR', ERROR: e, MSG: (e?e.message:'')};
         }  
     }
 
-    Note.prototype.fetchNotes = (accessToken, customerId) => {
+    Note.prototype.fetchNotes = (accessToken, customerId, includeArchived) => {
         return new Promise( async (resolve, reject) => {
             let userId = await utils.getStoreOwnerUserId(accessToken);
-            let sql = SQL.FETCH;
+            let sql = '';
+            if(includeArchived)
+                sql = SQL.FETCH_ALL;
+            else
+                sql = SQL.FETCH;
             sql = sql.replace(/REPLACE_USERID/g, userId);
             Note.dataSource.connector.query(sql, [customerId], (err, response) => {
                 if(err) {
@@ -43,6 +47,14 @@ module.exports = function(Note) {
                     return customerId;
                 },
                 description: 'customerId',
+            },
+            {
+                arg: 'include_archived', type: 'boolean', http: (ctx) => {
+                    let req = ctx && ctx.req;
+                    let includeArchived = req && req.query.include_archived;
+                    return includeArchived;
+                },
+                description: 'Include Archived Notes',
             }
         ],
         returns: {
@@ -106,6 +118,31 @@ module.exports = function(Note) {
         description: 'Update note'
     });
 
+    Note.remoteMethod('archiveNote', {
+        accepts: [{
+                arg: 'accessToken', type: 'string', http: (ctx) => {
+                    let req = ctx && ctx.req;
+                    let authToken = null;
+                    if(req && req.headers.authorization)
+                        authToken = req.headers.authorization || req.headers.Authorization;
+                    return authToken;
+                },
+                description: 'Access Token',
+            }, {
+                arg: 'params', type: 'object', default: {}, http: {source: 'body'}
+            }
+        ],
+        returns: {
+            type: 'object',
+            root: true,
+            http: {
+                source: 'body'
+            }
+        },
+        http: {path: '/archive-note', verb: 'patch'},
+        description: 'Archive note'
+    });
+
     Note.insertNewNote = (accessToken, params, cb) => {
         Note._insertNewNote(accessToken, params.customerId, params.custKey, params.content).then(
             (resp) => {
@@ -138,7 +175,7 @@ module.exports = function(Note) {
     }
 
     Note.updateNote = (accessToken, params, cb) => {
-        Note._updateNote(accessToken, params.content, params.noteId).then(
+        Note._updateNote(accessToken, params.content, params.noteId, params.customerId).then(
             (resp) => {
                 if(resp)
                     cb(null, {STATUS: 'SUCCESS', RESP: resp});
@@ -152,12 +189,43 @@ module.exports = function(Note) {
         );
     }
 
-    Note._updateNote = (accessToken, content, noteId) => {
+    Note._updateNote = (accessToken, content, noteId, customerId) => {
         return new Promise(async (resolve, reject) => {
             let userId = await utils.getStoreOwnerUserId(accessToken);
             let sql = SQL.UPDATE_NOTE;
             sql = sql.replace(/REPLACE_USERID/g, userId);
-            Note.dataSource.connector.query(sql, [content, noteId], (err, res) => {
+            Note.dataSource.connector.query(sql, [content, noteId, customerId], (err, res) => {
+                if(err) {
+                    console.log(err);
+                    return reject(err);
+                } else {
+                    return resolve(true);
+                }
+            });
+        })
+    }
+
+    Note.archiveNote = (accessToken, params, cb) => {
+        Note._archiveNote(accessToken, params.noteId, params.customerId).then(
+            (resp) => {
+                if(resp)
+                    cb(null, {STATUS: 'SUCCESS', RESP: resp});
+                else
+                    cb(null, {STATUS: 'ERROR', RESP: resp});
+            }
+        ).catch(
+            (e)=> {
+                cb({STATUS: 'EXCEPTION', ERR: e}, null);
+            }
+        );
+    }
+
+    Note._archiveNote = (accessToken, noteId, customerId) => {
+        return new Promise(async (resolve, reject) => {
+            let userId = await utils.getStoreOwnerUserId(accessToken);
+            let sql = SQL.ARCHIVE_NOTE;
+            sql = sql.replace(/REPLACE_USERID/g, userId);
+            Note.dataSource.connector.query(sql, [noteId, customerId], (err, res) => {
                 if(err) {
                     console.log(err);
                     return reject(err);
@@ -170,7 +238,9 @@ module.exports = function(Note) {
 };
 
 let SQL = {
-    FETCH: `SELECT * FROM notes_REPLACE_USERID WHERE CustomerId=?`,
+    FETCH_ALL: `SELECT * FROM notes_REPLACE_USERID WHERE CustomerId=?`,
+    FETCH: `SELECT * FROM notes_REPLACE_USERID WHERE CustomerId=? AND Archived=0`,
     INSERT_NEW_NOTE: `INSERT INTO notes_REPLACE_USERID (CustomerId, CustomerHashKey, Notes) VALUES (?, ?, ?)`,
-    UPDATE_NOTE: `UPDATE notes_REPLACE_USERID SET Notes=? WHERE Id=?`
+    UPDATE_NOTE: `UPDATE notes_REPLACE_USERID SET Notes=? WHERE Id=? AND CustomerId=?`,
+    ARCHIVE_NOTE: `UPDATE notes_REPLACE_USERID SET Archived=1 WHERE Id=? AND CustomerId=?`
 };
