9c5491c1303970b2366ccbbd92da53c93a7e30c9
lalithkumart <lalithkmr94@gmail.com>
2023-04-09 20:48:22 +0530

    jewellery tag setup api's

--- server/models/jewellery-tag-settings.js
+++ server/models/jewellery-tag-settings.js
@@ -38,6 +38,39 @@ module.exports = function(JewelleryTagSettings) {
         description: 'For fetching tag settings.',
     });
 
+    JewelleryTagSettings.remoteMethod('updateTagPreferenceApi', {
+        accepts: [
+            {
+                arg: 'accessToken', type: 'string', http: (ctx) => {
+                    let req = ctx && ctx.req;
+                    let accessToken;
+                    if(req && req.headers.authorization)
+                        accessToken = req.headers.authorization;
+                    return accessToken;
+                },
+                description: 'Authorization from header',
+            }, {
+                arg: 'payload',
+                type: 'object',
+                default: {
+                    
+                },
+                http: {
+                    source: 'body',
+                },
+            }
+        ],
+        returns: {
+            type: 'object',
+            root: true,
+            http: {
+                source: 'body'
+            }
+        },
+        http: {path: '/update-tag-selection', verb: 'put'},
+        description: 'Updates tag selection'
+    });
+
     JewelleryTagSettings._getSettings = (accessToken) => {
         return new Promise(async (resolve, reject) => {
             let _userId = await utils.getStoreOwnerUserId(accessToken);
@@ -47,6 +80,27 @@ module.exports = function(JewelleryTagSettings) {
                 } else {
                     resolve(res[0] || null);
                 }
+            });
+        });
+    }
+
+    JewelleryTagSettings.updateTagPreferenceApi = async (accessToken, payload) => {
+        try {            
+            if(!accessToken)
+                throw 'Access Token is missing';
+            await JewelleryTagSettings._updateTagPreference(accessToken, payload);
+            return {STATUS: 'SUCCESS', MESSAGE: 'UPDATED SUCCESSFULLY'};
+        } catch(e) {
+            return { STATUS: 'ERROR', MESSAGE: e}
+        }
+    }
+
+    JewelleryTagSettings._updateTagPreference = (accessToken, payload) => {
+        return new Promise(async (resolve, reject) => {
+            let _userId = await utils.getStoreOwnerUserId(accessToken);
+            JewelleryTagSettings.dataSource.connector.query(SQL.UPDATE_TAG_PREFERENCE, [payload.selectedTemplateId, _userId], (err, res) => {
+                if(err) return reject(err);
+                else return resolve(true);
             });
         });
     }
@@ -62,5 +116,6 @@ let SQL = {
                             LEFT JOIN
                         jewellery_tag_avl_template_list list ON settings.selected_tag_template_id = list.template_id
                     WHERE
-                        settings.user_id = ?`
+                        settings.user_id = ?`,
+    UPDATE_TAG_PREFERENCE: `UPDATE jewellery_tag_settings SET selected_tag_template_id=? WHERE user_id=?`
 }
--- server/models/jewellery_tag_avl_template_list.js
+++ server/models/jewellery_tag_avl_template_list.js
@@ -1,6 +1,65 @@
 'use strict';
 let app = require('../server.js');
+let utils = require('../utils/commonUtils');
 
 module.exports = function(JewelleryTagAvlTemplates) {
 
+    JewelleryTagAvlTemplates.remoteMethod('fetchListApiHandler', {
+        accepts: [
+            {
+                arg: 'accessToken', type: 'string', http: (ctx) => {
+                    let req = ctx && ctx.req;
+                    let accessToken;
+                    if(req && req.headers.authorization)
+                        accessToken = req.headers.authorization;
+                    return accessToken;
+                },
+                description: 'Arguments goes here',
+            }],
+        returns: {
+            type: 'object',
+            root: true,
+            http: {
+                source: 'body',
+            },
+        },
+        http: {path: '/fetch-list', verb: 'get'},
+        description: 'For fetching tag avl list.',
+    });
+    
+    JewelleryTagAvlTemplates.fetchListApiHandler = async (accessToken) => {
+        try {            
+            if(!accessToken)
+                throw 'Access Token is missing';
+            let tagTemplates = await JewelleryTagAvlTemplates._getTemplateList(accessToken);
+            return {STATUS: 'SUCCESS', TAG_TEMPLATES: tagTemplates};
+        } catch(e) {
+            return { STATUS: 'ERROR', MESSAGE: e}
+        }
+    };
+
+    JewelleryTagAvlTemplates._getTemplateList = async (accessToken) => {
+        return new Promise(async (resolve, reject) => {
+            let _userId = await utils.getStoreOwnerUserId(accessToken);
+            JewelleryTagAvlTemplates.dataSource.connector.query(SQL.FETCH_TAG_TEMPLATES, [_userId], (err, res) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            });
+        });
+    }
+
 };
+let SQL = {
+    FETCH_TAG_TEMPLATES: `SELECT 
+                            templates.template_id, 
+                            templates.screenshot_url,
+                            settings.selected_tag_template_id,
+                            settings.customization
+                        FROM
+                            gsprod.jewellery_tag_avl_template_list templates 
+                                LEFT JOIN
+                            gsprod.jewellery_tag_settings settings ON (settings.selected_tag_template_id = templates.template_id AND settings.user_id = ?)`,
+}
